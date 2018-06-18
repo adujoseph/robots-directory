d49783e24d6ff2c2068a2529c3fb4450ff4b4d69
AduJoseph <iamjosephadu@gmail.com>
2018-06-18 03:13:33 -0700

    almost done

--- src/Card.js
+++ src/Card.js
@@ -2,13 +2,13 @@ import React from 'react';
 // import ReactDOM from 'react-dom';
 // import App from './containers/App';
 
-const Card = () => {
+const Card = ({id,name,email}) => {
     return (
-        <div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+        <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
             <img src="https://robohash.org/test?200x200" alt="robot" />
             <div>
-                <h1 className="tc">Name</h1>
-                <p className="tc">email</p>
+                <h1 >{name}</h1>
+                <p >{email}</p>
             </div>
         </div>
     );
