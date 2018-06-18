5c20278cf8c390e613294d3a57bdd32a0bc01abc
AduJoseph <iamjosephadu@gmail.com>
2018-06-17 22:59:27 -0700

    create a new component

---
+++ src/Hello.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+import './App.css';
+
+class Hello extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Robots Directory...</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default Hello;
